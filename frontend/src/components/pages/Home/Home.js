63e28f65d2ca5f1e55162bad428a89b6aab579f7
Ivan Siderov <ivan.sideroff@gmail.com>
2020-05-04 01:16:39 +0300

    added: home

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import { Loading } from "../../atoms";
 
 import "./Home.scss";
@@ -16,25 +17,35 @@ export default function Home() {
       .then((response) => response.json())
       .then((categories) => {
         setCategories(categories.data);
+        setIsLoading(false);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      });
   }, []);
 
   return (
     <>
-      <div
-        className="parallax"
-        style={{ backgroundImage: "url('/hero.jpg')" }}
-      ></div>
-      <div className="categories-container">
+      <div className="parallax" style={{ backgroundImage: "url('/hero.jpg')" }}>
+        <div className="title">Damyan Georgiev Photography</div>
+        <div className="subtitle">
+          Photography is the story I fail to put into words
+        </div>
+      </div>
+      <div className="categories">
         {isLoading && <Loading />}
-        {categories &&
+
+        {!isLoading &&
+          categories &&
           categories.map((category) => (
-            <div>
-              <div>{category.title}</div>
-              <div>{category.description}</div>
-              <div>{category.primaryImage}</div>
-            </div>
+            <NavLink
+              to={`/category/${category.title.toLowerCase()}`}
+              className="category card"
+              style={{ backgroundImage: `url('${category.primaryImage}')` }}
+            >
+              <div className="title">{category.title}</div>
+            </NavLink>
           ))}
       </div>
     </>
