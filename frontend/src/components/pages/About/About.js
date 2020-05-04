40f0fda5351b24a73e92b716206f31679a02935f
Ivan Siderov <ivan.sideroff@gmail.com>
2020-05-04 22:09:05 +0300

    added: about-me

--- frontend/src/components/pages/About/About.js
+++ frontend/src/components/pages/About/About.js
@@ -2,16 +2,48 @@ import React from "react";
 
 import "./About.scss";
 
-//TODO: fix picture here
 export default function About() {
   return (
     <div className="about-me">
-      <div
-        className="picture"
-        style={{ backgroundImage: "url('/about-me.png')" }}
-      ></div>
+      <img className="picture" src="/about-me.png" alt="Damyan Georgiev" />
       <div className="text">
-        <h3>The man behind the camera</h3>
+        <h2>The man behind the camera</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          soluta facilis. Possimus facilis voluptas quo? Debitis eum sequi
+          repellendus nesciunt vitae quis, quibusdam error cum magni aut iusto
+          veritatis cupiditate. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Minima, soluta facilis. Possimus facilis voluptas
+          quo? Debitis eum sequi repellendus nesciunt vitae quis, quibusdam
+          error cum magni aut iusto veritatis cupiditate. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Minima, soluta facilis. Possimus
+          facilis voluptas quo? Debitis eum sequi repellendus nesciunt vitae
+          quis, quibusdam error cum magni aut iusto veritatis cupiditate.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          soluta facilis. Possimus facilis voluptas quo? Debitis eum sequi
+          repellendus nesciunt vitae quis, quibusdam error cum magni aut iusto
+          veritatis cupiditate. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Minima, soluta facilis. Possimus facilis voluptas
+          quo? Debitis eum sequi repellendus nesciunt vitae quis, quibusdam
+          error cum magni aut iusto veritatis cupiditate. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Minima, soluta facilis. Possimus
+          facilis voluptas quo? Debitis eum sequi repellendus nesciunt vitae
+          quis, quibusdam error cum magni aut iusto veritatis cupiditate.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          soluta facilis. Possimus facilis voluptas quo? Debitis eum sequi
+          repellendus nesciunt vitae quis, quibusdam error cum magni aut iusto
+          veritatis cupiditate. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Minima, soluta facilis. Possimus facilis voluptas
+          quo? Debitis eum sequi repellendus nesciunt vitae quis, quibusdam
+          error cum magni aut iusto veritatis cupiditate. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Minima, soluta facilis. Possimus
+          facilis voluptas quo? Debitis eum sequi repellendus nesciunt vitae
+          quis, quibusdam error cum magni aut iusto veritatis cupiditate.
+        </p>
       </div>
     </div>
   );
